fe422c4be8b8de6a22b4d6c5e8632efc5e821a0f
YaoYing <yaoying@YaoYingdeMacBook-Pro.local>
2015-09-11 20:56:41 +0800

    add im module

--- api/im.py
+++ api/im.py
@@ -25,13 +25,74 @@ class im(SpuRequestHandler):
         """
             发送即时通讯消息
         """
-        if user_id == 0 or sight_id == 0:
+        content = content.encode("utf8")
+        if from_user_id == 0 or to_user_id == 0:
             return self._response(Pyobject(Error.param_error))
 
-        sql = "insert into im(user_id,sight_id,create_on) values(%s,%s,now())" % (
-            user_id, 
-            sight_id)
+        sql = "insert into im(from_user_id,to_user_id,content,create_on) values(%s,%s,'%s',now())" % (
+            from_user_id, 
+            to_user_id,
+            content)
         data = mysql_conn.execsql(sql)
         return self._response(Pyobject(Error.success, data))
-        
+
+    def getContacts(self,
+                    user_id={"atype":int, "adef": 0}
+    ):
+        """
+            获取最近联系人列表
+        """
+        if user_id == 0:
+            return self._response(Pyobject(Error.param_error))
+
+        sql = "select * from (select to_user_id,user.username to_user_name, max(create_on)create_on,(select content from im temp where temp.from_user_id = %s and temp.to_user_id = main.to_user_id order by id desc limit 1)content from im main left join user on user.id=main.to_user_id where from_user_id = %s group by to_user_id)t order by id desc" % (user_id, user_id)
+        data = mysql_conn.query(sql)
+
+        for i in xrange(0,len(data)):
+            data[i]['create_on'] = str(data[i]['create_on'])
+
+        return self._response(Pyobject(Error.success, data))
+
+    def getContactContent(self,
+                          from_user_id={"atype": int, "adef": 0},
+                          to_user_id={"atype": int, "adef": 0},
+                          last_id={"atype": int, "adef": 0}
+    ):
+        """
+            获取最新的聊天内容
+        """
+        if from_user_id == 0 or to_user_id == 0:
+            return self._response(Pyobject(Error.param_error))
+
+        sql = "select * from im where ((from_user_id = %s and to_user_id = %s) or (from_user_id = %s and to_user_id = %s))" % (from_user_id,to_user_id,to_user_id,from_user_id)
+
+        if last_id != 0:
+            sql += " and id > %s" % last_id
+
+        sql += " order by id desc limit 10"
+
+        data = mysql_conn.query(sql)
+        for i in xrange(0,len(data)):
+            data[i]['create_on'] = str(data[i]['create_on'])
+        return self._response(Pyobject(Error.success, data))
+
+    def index(self,
+              from_user_id={"atype": int, "adef": 0},
+              to_user_id={"atype": int, "adef": 0}
+    ):
+        """
+            聊天页面
+        """
+        sql = "select a.username from_user_name,b.username to_user_name from user a,user b where a.id = %s and b.id = %s limit 1" % (from_user_id, to_user_id)
+
+        data = mysql_conn.query(sql)
+
+        output = {}
+        if len(data) > 0:
+            output['from_user_name'] = data[0]['from_user_name'].encode("utf8")
+            output['to_user_name'] = data[0]['from_user_name'].encode("utf8")
+            output['from_user_id'] = from_user_id
+            output['to_user_id'] = to_user_id
+
+        self._html_render("im.html", output)
 
