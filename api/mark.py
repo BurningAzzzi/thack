2a62108696ca24955f0908a93ac9808e70225099
eleven <iGod_eleven@163.com>
2015-09-13 03:05:17 +0800

    add info window of viewer

--- api/mark.py
+++ api/mark.py
@@ -7,6 +7,10 @@
 import json
 from api.base import SpuLogging, SpuRequestHandler, Error, Pyobject, PyobjectList, POST
 from sputnik.SpuDB import SpuDBManager
+from util import to_utf8
+import top.api
+
+top.setDefaultAppInfo("23012937", "20a702ee2816e5ae3b7f1e637621fcf6")
 
 mysql_conn = SpuDBManager.get_spudb()
 
@@ -73,7 +77,14 @@ class mark(SpuRequestHandler):
         """总体情况"""
         sql = "select * from mark where user_id = %s" % user_id
         data = mysql_conn.query(sql)
+        sight_ctrl = top.api.TripScenicGetRequest()
         for i in xrange(0,len(data)):
             data[i]['create_on'] = str(data[i]['create_on'])
+            data[i]["mark_time"] = "时间:" + data[i]["create_on"]
+            sight_id = data[i]["sight_id"]
+            sight_ctrl.scenic_id = sight_id
+            res = sight_ctrl.getResponse()
+            sight_name = res["trip_scenic_get_response"]["travel_scenic"]["name"]
+            data[i]["address"] = to_utf8(sight_name)
         return self._html_render("viewer.html", {"markers": data, "user_id": user_id})
 
